d294b953eb9076e49876d4f9f71d7e3b125e0f04
Daniel Shumway <shumway.danny@gmail.com>
2015-07-21 22:22:11 -0400

    Initial setup

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var client = require('src/index'); //Note, if this isn't getting imported, try double quotes?
+
+//report crashes.
+require('crash-reporter').start();
+
+//Quit on close.
+app.on('window-all-closed', function() {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+//Window will be be closed when this is GBC
+var mainWindow = null;
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.openDevTools();
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+})
---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function Stream(gameboy, canvas) {
+    var _this = this;
+    _this.properties = {
+        //Stuff here.
+    }
+}
+
+function newLoop() {
+
+
+}
